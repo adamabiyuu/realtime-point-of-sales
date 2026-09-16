02680799e6f8f651b8aacde044f6acddb76d4791
adamabiyuu <advmabiyu@gmail.com>
2026-09-16 09:33:24 +0700

    fix: login

--- src/app/(auth)/login/_components/login.tsx
+++ src/app/(auth)/login/_components/login.tsx
@@ -3,60 +3,112 @@
 import FormInput from "@/components/common/form-input";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { INITIAL_LOGIN_FORM } from "@/constants/auth-constant";
+import { INITIAL_LOGIN_FORM, INITIAL_STATE_LOGIN_FORM } from "@/constants/auth-constant";
 import { LoginForm, loginSchemaForm } from "@/validations/auth-validation";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { startTransition, useActionState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-
+import { login } from "../actions";
 export default function Login() {
   const form = useForm<LoginForm>({
     resolver: zodResolver(loginSchemaForm),
     defaultValues: INITIAL_LOGIN_FORM,
   });
+  const [loginState, loginAction, isPendingLogin] = useActionState(login, INITIAL_STATE_LOGIN_FORM);
 
-  const onSubmit = form.handleSubmit(async (data) => {});
-
+  const onSubmit = form.handleSubmit(async (data) => {
+    const formData = new FormData();
+    Object.entries(data).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+    startTransition(() => {
+      loginAction(formData);
+    });
+  });
+  useEffect(() => {
+    if (loginState?.status === "error") {
+      startTransition(() => {
+        loginAction(null);
+      });
+    }
+  }, [loginState, loginAction]);
   return (
     <Card>
+      {" "}
       <CardHeader className="text-center">
-        <CardTitle className="text-xl">Welcome</CardTitle>
-        <CardDescription>Login to access all features</CardDescription>
-      </CardHeader>
-
+        {" "}
+        <CardTitle className="text-xl">Welcome</CardTitle> <CardDescription>Login to access all features</CardDescription>{" "}
+      </CardHeader>{" "}
       <CardContent>
+        {" "}
         <form onSubmit={onSubmit} className="space-y-4">
-          <FormInput form={form} name="email" label="Email" placeholder="Insert email here" type="email" />
-
-          <FormInput form={form} name="password" label="Password" placeholder="******" type="password" />
-
-          <Button type="submit">Login</Button>
-        </form>
-      </CardContent>
+          {" "}
+          <FormInput form={form} name="email" label="Email" placeholder="Insert email here" type="email" /> <FormInput form={form} name="password" label="Password" placeholder="******" type="password" />{" "}
+          <Button type="submit" disabled={isPendingLogin}>
+            {" "}
+            {isPendingLogin ? <Loader2 className="animate-spin" /> : "Login"}{" "}
+          </Button>{" "}
+        </form>{" "}
+      </CardContent>{" "}
     </Card>
   );
 }
 
 // punya mentor
 
-// ("use client");
+// 'use client';
 
-// import FormInput from "@/components/common/form-input";
-// import { Button } from "@/components/ui/button";
-// import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-// import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-// import { Input } from "@/components/ui/input";
-// import { INITIAL_LOGIN_FORM } from "@/constants/auth-constant";
-// import { LoginForm, loginSchema } from "@/validations/auth-validation";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { useForm } from "react-hook-form";
+// import FormInput from '@/components/common/form-input';
+// import { Button } from '@/components/ui/button';
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardHeader,
+//   CardTitle,
+// } from '@/components/ui/card';
+// import { Form } from '@/components/ui/form';
+// import {
+//   INITIAL_LOGIN_FORM,
+//   INITIAL_STATE_LOGIN_FORM,
+// } from '@/constants/auth-constant';
+// import { LoginForm, loginSchemaForm } from '@/validations/auth-validation';
+// import { zodResolver } from '@hookform/resolvers/zod';
+// import { startTransition, useActionState, useEffect } from 'react';
+// import { useForm } from 'react-hook-form';
+// import { login } from '../actions';
+// import { Loader2 } from 'lucide-react';
 
 // export default function Login() {
 //   const form = useForm<LoginForm>({
-//     resolver: zodResolver(loginSchema),
+//     resolver: zodResolver(loginSchemaForm),
 //     defaultValues: INITIAL_LOGIN_FORM,
 //   });
 
-//   const onSubmit = form.handleSubmit(async (data) => {});
+//   const [loginState, loginAction, isPendingLogin] = useActionState(
+//     login,
+//     INITIAL_STATE_LOGIN_FORM,
+//   );
+
+//   const onSubmit = form.handleSubmit(async (data) => {
+//     const formData = new FormData();
+//     Object.entries(data).forEach(([key, value]) => {
+//       formData.append(key, value);
+//     });
+
+//     startTransition(() => {
+//       loginAction(formData);
+//     });
+//   });
+
+//   useEffect(() => {
+//     if (loginState?.status === 'error') {
+//       startTransition(() => {
+//         loginAction(null);
+//       });
+//     }
+//   }, [loginState]);
 
 //   return (
 //     <Card>
@@ -67,9 +119,23 @@ export default function Login() {
 //       <CardContent>
 //         <Form {...form}>
 //           <form onSubmit={onSubmit} className="space-y-4">
-//             <FormInput form={form} name="email" label="Email" placeholder="Insert email here" type="email" />
-//             <FormInput form={form} name="password" label="Password" placeholder="******" type="password" />
-//             <Button type="submit">Login</Button>
+//             <FormInput
+//               form={form}
+//               name="email"
+//               label="Email"
+//               placeholder="Insert email here"
+//               type="email"
+//             />
+//             <FormInput
+//               form={form}
+//               name="password"
+//               label="Password"
+//               placeholder="******"
+//               type="password"
+//             />
+//             <Button type="submit">
+//               {isPendingLogin ? <Loader2 className="animate-spin" /> : 'Login'}
+//             </Button>
 //           </form>
 //         </Form>
 //       </CardContent>
