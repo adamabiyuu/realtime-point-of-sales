f0ffdbd57623b810e83d72154958dce68c9f49d2
adamabiyuu <advmabiyu@gmail.com>
2026-09-24 08:52:32 +0700

    feat: pagination

--- src/components/common/data-table.tsx
+++ src/components/common/data-table.tsx
@@ -1,8 +1,27 @@
 import { ReactNode } from "react";
 import { Card } from "../ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
+import PaginationDataTable from "./pagination-data-table";
 
-export default function DataTable({ header, data, isLoading }: { header: string[]; data: (string | ReactNode)[][]; isLoading?: boolean }) {
+export default function DataTable({
+  header,
+  data,
+  isLoading,
+  totalPages,
+  currentPage,
+  currentLimit,
+  onChangePage,
+  onChangeLimit,
+}: {
+  header: string[];
+  data: (string | ReactNode)[][];
+  isLoading?: boolean;
+  totalPages: number;
+  currentPage: number;
+  currentLimit: number;
+  onChangePage: (page: number) => void;
+  onChangeLimit: (limit: number) => void;
+}) {
   return (
     <div className="w-full flex flex-col gap-4">
       <Card className="p-0">
@@ -43,6 +62,14 @@ export default function DataTable({ header, data, isLoading }: { header: string[
           </TableBody>
         </Table>
       </Card>
+      <div className="flex items-center justify-between">
+        <div></div>
+        {totalPages > 1 && (
+          <div className="flex justify-end">
+            <PaginationDataTable currentPage={currentPage} onChangePage={onChangePage} totalPages={totalPages} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
